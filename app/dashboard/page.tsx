8e72c6bc7b3e9159393450114bb8578a4547e809
Serhii Kuznietsov <153386769+Sergio-1n@users.noreply.github.com>
2024-12-02 19:26:48 +0100

    Add files via upload

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+
+// const Dashboard = () => {
+//   return <div>dashboard</div>;
+// };
+
+// export default Dashboard;
+export default function Page() {
+  return <p>Dashboard Page</p>;
+}
